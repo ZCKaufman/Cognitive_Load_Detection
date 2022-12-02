d89afd2b0643b6cce210d4ee219b3dae5895345b
Zachary Kaufman <zckaufman@Zacharys-MacBook-Pro-2.local>
2022-12-02 13:44:20 -0500

    Corrected bugs and created data output to specifications for our MatLab code

--- GSR_Code/GSR_Code.ino
+++ GSR_Code/GSR_Code.ino
@@ -1,13 +1,37 @@
 const int GSR=A0;
 int sensorValue=0;
 int gsr_average=0;
- 
+
+
+//import processing.serial.*;
+//Serial mySerial;
+//PrintWriter output;
+
+void setup() {
+
+}
+
 void setup(){
   Serial.begin(9600);
+//  mySerial = new Serial( this, Serial.list()[0], 9600 );
+//output = createWriter( "data.txt" );
 }
- 
+
+//void draw(){
+//  if (mySerial.available() > 0 ) {
+//      String value = mySerial.readString();
+//      if ( value != null ) {
+//      output.println(value);
+//      }
+//    }
+//}
+
+
+
 void loop(){
   long sum=0;
+  long c_sum=0;
+  long time = 0;
   for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
       {
       sensorValue=analogRead(GSR);
@@ -15,5 +39,28 @@ void loop(){
       delay(5);
       }
    gsr_average = sum/10;
+   c_sum += gsr_average;
+   time_stamp += 1; // 1 timestep for every 10 that are averaged. 
+   Serial.print("GSR Value:"); //comment this out for processing
    Serial.println(gsr_average);
+   Serial.print("Cumulative sum:"); //comment this out for processing
+   Serial.println(c_sum);
+   gsr_overall_avg = c_sum/time_stamp;
+   Serial.print("Average"); //comment this out for processing  
+   Serial.print(gsr_overall_avg);
+   
+}
+
+//open COM4 in coolterm 
+//connection - capture to text/binary file 
+//* option to save file 
+//Serial.txt 
+//connect data will be saved in the coolterm 
+
+
+
+void keyPressed() {
+  output.flush(); // Writes the remaining data to the file
+  output.close(); // Finishes the file
+  exit(); // Stops the program
 }
--- main/main.ino
+++ main/main.ino
@@ -3,19 +3,20 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
   
-Adafruit_BNO055 bno = Adafruit_BNO055(55);
+Adafruit_BNO055 IMU = Adafruit_BNO055(55);
 const int GSR=A0;
 int gsrSensorValue=0;
-int gsr_average=0;
+int ten_sample_average=0;
+long gsrOverallAvg=0;
+long c_sum=0;
+long time_stamp = 0;
 
-void setup(void) 
-{
+void setup(void) {
   Serial.begin(9600);
   Serial.println("Orientation Sensor Test"); Serial.println("");
   
   /* Initialise the sensor */
-  if(!bno.begin())
-  {
+  if(!IMU.begin()) {
     /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
@@ -23,35 +24,54 @@ void setup(void)
   
   delay(1000);
     
-  bno.setExtCrystalUse(true);
+  IMU.setExtCrystalUse(true);
+
+  // First row in CSV
+  Serial.println("10 Sample Average, Cumulative Sum, GSR Overall Average, Gyro X, Gyro Y, Gyro Z, Acc X, Acc Y, Acc Z");
 }
 
 void loop(void) 
 {
   /* Get a new sensor event */ 
   sensors_event_t event; 
-  bno.getEvent(&event);
-  
+  long sum = 0;
+
+  // IMU Data Collection
+  IMU.getEvent(&event);
+  imu::Vector<3> acc = IMU.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu::Vector<3> gyro = IMU.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+
+  // GSR Data
+  for(int i=0;i<10;i++) {
+      gsrSensorValue = analogRead(GSR);
+      sum += gsrSensorValue;
+      delay(5);
+  }
+  ten_sample_average = sum/10;
+  c_sum += ten_sample_average;
+  time_stamp += 1;
+  gsrOverallAvg = c_sum/time_stamp;
+
   /* Display the floating point data */
-  Serial.print("X: ");
-  Serial.print(event.orientation.x, 4);
-  Serial.print("\tY: ");
-  Serial.print(event.orientation.y, 4);
-  Serial.print("\tZ: ");
-  Serial.print(event.orientation.z, 4);
+  //Serial.print("GSR: ");
+  Serial.print(ten_sample_average);
+  Serial.print(", ");
+  Serial.print(c_sum);
+  Serial.print(", ");
+  Serial.print(gsrOverallAvg);
+  Serial.print(", ");
+  Serial.print(event.gyro.x, 4);
+  Serial.print(", ");
+  Serial.print(event.gyro.y, 4);
+  Serial.print(", ");
+  Serial.print(event.gyro.z, 4);
+  Serial.print(", ");
+  Serial.print(event.acceleration.x, 4);
+  Serial.print(", ");
+  Serial.print(event.acceleration.y, 4);
+  Serial.print(", ");
+  Serial.print(event.acceleration.z, 4);
   Serial.println("");
   
   delay(100);
-}
-
-void loop(){
-  long sum=0;
-  for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
-      {
-      sensorValue=analogRead(GSR);
-      sum += sensorValue;
-      delay(5);
-      }
-   gsr_average = sum/10;
-   Serial.println(gsr_average);
 }
